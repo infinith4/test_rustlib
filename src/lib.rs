f3e80f83981f8e32de2219dbad9d5213b11282b7
infinith4 <infinith4@gmail.com>
2022-12-28 23:45:27 +0900

    add lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+mod generator;
+
+pub fn print_random_number(){
+    let n = generator::gen_ran();
+    println!("Random u8: {}", n);
 }
